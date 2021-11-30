f3017dc10f9a039b8acd1725db1fb8b33e974d16
gondecu <gon.dc98@gmail.com>
2021-11-29 22:05:23 -0300

    my first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname + "/views/home.html"))
+});
+
+
+
+
+
+
+app.listen(process.env.PORT || 2000, ()=>{
+    console.log("servidor funcionando 2000")
+});
